5652a29ea0ce571e6b72131b8dd652b567922f1b
James Banks <jabanks@comcast.net>
2011-08-26 20:45:57 -0600

    Working on names and such.

--- lexer.h
+++ lexer.h
@@ -3,15 +3,15 @@
 
 
 
-enum lexer_state {
-    lexer_state_mask = 0xe,         /* binary 1110 */
-    lexer_escape_mask = 0x1,        /* binary 0001 */
-    lexer_initial = 0,
-    lexer_string = 0x2,             /* binary 0010 */
-    lexer_string_escape = 0x3,      /* binary 0011 */
-    lexer_quoted = 0x4,             /* binary 0100 */
-    lexer_quoted_escape = 0x5,      /* binary 0101 */
-    lexer_blurb = 0x6               /* binary 0110 */
+enum stream_state {
+    stream_state_mask = 0xe,        /* binary 1110 */
+    stream_escape_mask = 0x1,       /* binary 0001 */
+    stream_start = 0,
+    stream_simple = 0x2,            /* binary 0010 */
+    stream_simple_escape = 0x3,     /* binary 0011 */
+    stream_quoted = 0x4,            /* binary 0100 */
+    stream_quoted_escape = 0x5,     /* binary 0101 */
+    stream_blurb = 0x6              /* binary 0110 */
 }
 
 
@@ -21,16 +21,8 @@ extern int lexer_handle_escape(
     pod_char_t *r,
     int *finished
 );
-extern int lexer_handle_initial(
-    pod_stream *stream,
-    pod_char_t c,
-    int *next_state
-);
-extern int lexer_handle_string(
-    pod_stream *stream,
-    pod_char_t c,
-    int *next_state
-);
+extern int scan_start(pod_stream *stream, pod_char_t c, int *next_state);
+extern int scan_string(pod_stream *stream, pod_char_t c, int *next_state);
 extern int lexer_handle_quoted(
     pod_stream *stream,
     pod_char_t c,
