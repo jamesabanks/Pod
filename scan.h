c58b0b4c2ed7d041fcb72259ae18232ab2b5c369
James Banks <jabanks@comcast.net>
2011-09-15 22:16:34 -0600

    Renamed scanner.h to scan.h

---
+++ scan.h
@@ -0,0 +1,26 @@
+#ifndef INCLUDE_POD_SCAN_H
+#define INCLUDE_POD_SCAN_H
+
+
+
+enum boolean {
+    false = 0,
+    true = 1
+};
+
+
+
+extern int scan_start(pod_stream *stream, pod_char_t c);
+extern int scan_escape(pod_stream *stream, pod_char_t c);
+extern int scan_escape_hex(pod_stream *stream, pod_char_t c);
+extern int scan_simple(pod_stream *stream, pod_char_t c);
+extern int scan_quoted(pod_stream *stream, pod_char_t c);
+extern int scan_blurb(pod_stream *stream, pod_char_t c);
+extern int scan_blurb_pre_size(pod_stream *stream, pod_char_t c);
+extern int scan_blurb_size(pod_stream *stream, pod_char_t c);
+extern int scan_blurb_post_size(pod_stream *stream, pod_char_t c);
+
+
+
+
+#endif /* INCLUDE_POD_SCAN_H */
