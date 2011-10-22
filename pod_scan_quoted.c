5da1ce1d68c95b8e8a8200b32789962dc8386596
James Banks <james@Small-Fry.local>
2011-10-21 22:41:10 -0600

    Updated scan_quoted to pod_scan_quoted

--- pod_scan_quoted.c
+++ pod_scan_quoted.c
@@ -1,43 +1,44 @@
-#include "scan.h"
+#include "pod_char.h"
+#include "pod_scan.h"
 
 
 
-    // scan_quoted
+    // pod_scan_quoted
     //
     // Pod has the opening quote.
     //
     // Returns:
     //      int     The error id of any problem that occurred (0 = no error)
     
-int scan_quoted(pod_stream *stream, pod_char_t c)
+int pod_scan_quoted(pod_stream *stream, pod_char_t c)
 {
     int warning;
 
     warning = 0;
     switch (c) {
-        case '\n': // Ends a quoted string
-        case '\r':
+        case POD_CHAR_NEWLINE: // Ends a quoted string
+        case POD_CHAR_RETURN:
             // Error: missing the '"' which ends the quoted string.
-            warning = 1;
-            stream->state = pod_start;
+            warning = 1; // TODO
+            stream->s_state = POD_STATE_START;
             break;
-        case '"':  // A quote ends the quoted string.
-            stream->state = pod_start;
+        case POD_CHAR('"'):  // A quote ends the quoted string.
+            stream->s_state = POD_STATE_START;
             break;
-        case '\\':
-            stream->state = pod_quoted_escape;
+        case POD_CHAR('\\'):
+            stream->s_state = POD_STATE_QUOTED_ESCAPE;
             break;
-        case '':
-            if (!pod_string_is_empty(stream->buffer)) {
-                pod_stream_add_token(stream, pod_token_string);
+        case POD_CHAR_EOB:
+            if (!pod_string_is_empty(stream->s_buffer)) {
+                pod_stream_add_token(stream, POD_TOKEN_STRING);
             }
-            pod_stream_add_token(stream, pod_token_pod_sync);
-            stream->state = pod_start;
+            pod_stream_add_token(stream, POD_TOKEN_POD_SYNC);
+            stream->s_state = POD_STATE_START;
             // Error: missing the '"' which ends the quoted string.
             warning = 1;
             break;
         default:
-            pod_string_append_char(stream->buffer, c);
+            pod_string_append_char(stream->s_buffer, c);
             break;
     }
 
